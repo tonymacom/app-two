c7f14efa93e079b8df88778f2f2e6279e4148749
tony-Ma-yami <tony.ma@yamibuy.com>
2018-11-17 13:51:15 +0800

    guava Splitter测试

--- src/test/java/com/tony/unit/AppTest.java
+++ src/test/java/com/tony/unit/AppTest.java
@@ -1,8 +1,12 @@
 package com.tony.unit;
 
+import com.google.common.base.Splitter;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -42,5 +46,20 @@ public class AppTest
         System.out.println((int)(52/2));
         System.out.println(100/0.85);
         System.out.println((int)(100/0.85));
+    }
+
+    @org.junit.Test
+    public void testsplit(){
+        String et = "a,b,,,,,c,d,,,,,,";
+        List<String> list = Arrays.asList(et.split(","));
+        System.out.println(list.size());
+        list.forEach(e -> System.out.println(e));
+
+        // guava Splitter
+        List<String> list2 = Splitter.on(",").splitToList(et);
+        System.out.println(list2.size());
+        list2.forEach(e -> System.out.println(e));
+
+
     }
 }
